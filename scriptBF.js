8533d7822f6ffb63cbbd072e17fc2eb22afb27f8
Blake Fogle <foglebd@gmail.com>
2019-10-29 21:23:52 -0700

    cards are DONE

--- scriptBF.js
+++ scriptBF.js
@@ -1,7 +1,8 @@
+
 function displayBookPreview(){
 
 var apiKeyGoogle = "AIzaSyBzrVa4fIYwGQhY0ZJKd3knqqKAmXrp1IM"
-var searchTitle = "the hobbit"
+var searchTitle = $("#search").val().trim();
 var numberOfBooks = 5
 var queryURL = "https://www.googleapis.com/books/v1/volumes?q=" + searchTitle + "&key=" + apiKeyGoogle;
 
@@ -11,34 +12,48 @@ $.ajax({
     }).then(function(response)
     {
     console.log(response)
-    console.log(response.items[0].volumeInfo.imageLinks.thumbnail)
+    console.log(response.items[0].volumeInfo.imageLinks.smallThumbnail)
     for (var i =0; i < numberOfBooks; i++){
         var cardWrapper = $("<div>");
         var bookContainer = $("<div>");
+        var bookImageDiv = $("<div>");
+        var previewButton = $("<a>");
+        var cardContent = $('<div>');
         var title = response.items[i].volumeInfo.title;
         var authors = response.items[i].volumeInfo.authors[0];
-        var coverArt = response.items[i].volumeInfo.imageLinks.thumbnail;
+        var coverArt = response.items[i].volumeInfo.imageLinks.smallThumbnail;
         var isbn = response.items[i].volumeInfo.industryIdentifiers[0].identifier;
 
-        cardWrapper.addClass("card-wrapper col s2");
-        bookContainer.addClass("container card");
-        bookContainer.attr("data-name", isbn);
+        cardWrapper.addClass("col s4 m2");
+        bookContainer.addClass("card");
+        bookImageDiv.addClass("card-image");
+        previewButton.addClass("btn-floating halfway-fab waves-effect waves-light red");
+        previewButton.attr("data-name", title);
+        cardContent.addClass("card-content");
+        // Appending the elements to the individual card classes
+        previewButton.append($('<i class="material-icons">more_horiz</i>'));
+        cardContent.append($('<h4 style="font-size:1.3rem;">' + title + '</h4>'));
+        cardContent.append($('<p style="font-style:italic;">' + authors + '</p>'));
+        bookTitle.append($(title));
+        // Appending the card classes inside their parent card Divs
+        
+        bookImageDiv.append($('<img src=' + coverArt +'>'));
+        bookImageDiv.append(previewButton);
 
-        bookContainer.append($('<img src=' + coverArt +'>'));
-        bookContainer.append($('<h4 style="font-size:1.3rem;text-align:center;">' + title + '</h4>'));
-        bookContainer.append($('<p style="font-style:italic;text-align:center;">' + authors + '</p>'));
-        bookContainer.append($('<p style="text-align:center;">' + isbn +'</p>'));
+        bookContainer.append(bookImageDiv);
+        bookContainer.append(cardContent);
+
         cardWrapper.append(bookContainer);
-        $('#book-cards').append(cardWrapper);
+
+        $('#book-results').append(cardWrapper);
+
+        // bookContainer.append($('<p style="text-align:center;">' + isbn +'</p>'));
+        // bookContainer.attr("daTitle", title);
         };
     });
 }
 
-
-$(document).on("click", ".book-search", displayBookPreview);
-
-//  input.addEventListener('keyup',function(e){
-//    if (e.keyCode === 13) {
-// alert('hi');
-//   }
-//  });
+$("form").submit(function(event) {
+    event.preventDefault();
+    displayBookPreview();
+    });
